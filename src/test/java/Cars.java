62c11f9b3692561cdee05b713d27ee02ba835f35
Justin dao <justinnhatdao@gmail.com>
2021-05-02 20:36:39 -0700

    5/2/2021

--- src/test/java/Cars.java
+++ src/test/java/Cars.java
@@ -1,23 +1,35 @@
 public class Cars{
 
-    public static class Audi extends Car{
+    public static class Carz{
 
         public static void main(String[] args) {
-            print_details();
+            //Audi
             int speed = 150;
             int seat = 3;
+            int wheel = 4;
             Audi A = new Audi();
-            System.out.println("Color Of Audi = "+A.color);
-            System.out.println("Fuel Of Audi = "+A.getfuel());
-            System.out.println("Wheels Of Audi = "+wheels);
+            System.out.println("Color Of Audi = Blue ");
+            System.out.println("Fuel Of Audi = Gasoline");
+            System.out.println("Wheels Of Audi = "+wheel);
             System.out.println("Speed Of Audi = "+speed);
             System.out.println("Car Seats = "+seat);
+            //Breaker in between
+            System.out.println("******************************************");
+            //Toyota car
+            int speeds=140;
+            int seats = 4;
+            int wheels =4;
+            Toyota B = new Toyota();
+            System.out.println("Color Of Toyota = Red ");
+            System.out.println("Fuel Of Toyota = Hydrogen");
+            System.out.println("Wheels Of Toyota = " + wheels);
+            System.out.println("Speed Of Toyota = " + speeds);
+            System.out.println("Car Seats = "+seats);
         }
     }
     public static class Toyota extends Car2 {
 
         public static void main(String[] args) {
-            print_details();
             int speed=140;
             int seat = 4;
             Toyota A = new Toyota();
@@ -27,10 +39,5 @@ public class Cars{
             System.out.println("Speed Of Toyota = " + speed);
             System.out.println("Car Seats = "+seat);
         }
-    }
-    public static void print_details(){
-        System.out.println(Toyota);
-        System.out.println("**********************");
-        System.out.println(Audi);
     }
 }
