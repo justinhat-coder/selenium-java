c4f1831943327d3663d47cc5b75857f94be80f1b
Justin dao <justinnhatdao@gmail.com>
2021-05-05 16:51:56 -0700

    5/5/21

--- src/test/java/Collage2.java
+++ src/test/java/Collage2.java
@@ -1,39 +1,28 @@
-public class College1 {
+public class Collage2 {
 
-    public static String Collage_Name = "A1 Collage";
-
-
-    private String Department = "Computer Engineering";
+    private String Department = "Mechanical Engineering";
     private String name;
     private double percentile;
     public static void main(String[] args) {
-
-        College1 student1 = new College1("Robert");
-        student1.setPercentage(67.32);
+        Collage2 student1 = new Collage2("Smith");
+        student1.setPercentage(57.35);
         student1.print_details();
-
-        College1 student2 = new College1("Alex");
-        student2.setPercentage(72.95);
-        student2.print_details();
     }
-    public College1(String student_name){//Constructor
-
+    public Collage2(String student_name){
         name = student_name;
     }
-    public void setPercentage(double perc){
 
+    public void setPercentage(double perc){
         percentile = perc;
     }
 
     public void print_details(){
         int Year = 2014;
         System.out.println("Resultg Of Year = "+Year);
-        System.out.println("Student's Collage Name = "+Collage_Name);
+        System.out.println("Student's Collage Name = "+ College1.Collage_Name);
         System.out.println("Student's Department = "+Department);
         System.out.println("Student's Name = "+name);
         System.out.println("Student's percentile = "+percentile+"%");
         System.out.println("**********************");
     }
-
 }
-
